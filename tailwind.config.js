79e0871670a98aa96f46a3cfcb918530d3b477d6
Marcus Vinicius Leite <marcus.leite.ext@rendimento.com.br>
2022-03-22 17:24:46 -0300

    feat: compre agora, resultado, premiação

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'th-green':'#01A78D'
+        'th-green':'#01A78D',
+        'th-dark-green': '#056052',
+        'th-blue':'#0D2A4A'
       }
     },
   },
