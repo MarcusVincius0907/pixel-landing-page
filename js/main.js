a8a30f4cc7d4176fd9058caf3f4f35e6f069b5ed
Marcus Vinicius Leite <marcus.leite.ext@rendimento.com.br>
2022-03-25 17:04:33 -0300

    feat: sobre nos, terms, js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function readMore(index){
+
+  const idBtn = `#read-more-${index}`;
+  const idText = `#text-read-more-${index}`;
+
+  let btn  = document.querySelector(idBtn);
+  let btnText = document.querySelector(idBtn + '> span > span');
+  let btnChevron = document.querySelector(idBtn + '> span > i');
+  let text = document.querySelector(idText);
+
+  if(btnText.textContent == 'Ler mais'){
+    btnText.innerHTML = 'Ler menos'
+    btnChevron.classList.remove('fa-chevron-down')
+    btnChevron.classList.add('fa-chevron-up')
+    text.classList.remove('h-24');
+    text.classList.add('h-full');
+  }else{
+    btnText.innerHTML = 'Ler mais'
+    btnChevron.classList.remove('fa-chevron-up')
+    btnChevron.classList.add('fa-chevron-down')
+    text.classList.add('h-24');
+    text.classList.remove('h-full');
+  }
+
+}
